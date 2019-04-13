20e80c95b311d1a34914c938d32df6a7327126b5
Paul Davis <pauldavis@Pauls-MacBook-Pro.local>
2019-04-13 13:23:10 -0400

    testing timer

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,18 +1,17 @@
 $(document).ready(function() {
-var question = $('#question') // question and answer HTML divs
-    answer1Div = $('#answer1')
-    answer2Div = $('#answer2')
-    answer3Div = $('#answer3')
-    answer4Div = $('#answer4')
-    answer = $('.answerRow')
-    question = $('#question')
-    display = $('#display')
+    var question = $('#question') // question and answer HTML divs
+        answer1Div = $('#answer1')
+        answer2Div = $('#answer2')
+        answer3Div = $('#answer3')
+        answer4Div = $('#answer4')
+        answer = $('.answerRow')
+        display = $('#display')
 
-    q = 4 // question number
-    wrong = 0 // wrong asnwers score
-    right = 0 // right answers score
-    time = 0 // time tracker
-    timer;
+        q = 5 // question number
+        wrong = 0 // wrong asnwers score
+        right = 0 // right answers score
+        time = 0 // time tracker
+        timer;
 
 
     questionAnswer = { // object full of Q and A values
@@ -59,7 +58,10 @@ var question = $('#question') // question and answer HTML divs
     }
 
     //click to start
-    $(document).one().click(function() {
+    $(document).one().click(function() { start() })
+
+    function start() {
+        q=1
         $(document).off('click') // click to start off on start
         answer.addClass('answerHover') // add green hover to answer on start
         question.addClass('typewriter') // add typewriter to question on start
@@ -67,31 +69,32 @@ var question = $('#question') // question and answer HTML divs
             answer.removeClass('hidden') // unhide questions after typing question
         }, 4500)
         setUp() // run setup function
-    })
+    }
 
 
     function setUp() {
-    timerFunc()
+        timerFunc()
 
-    setTimeout(function() {
-        answer.removeClass('hidden') }, 4500) // load in answers after typing animation
+        setTimeout(function() {
+            answer.removeClass('hidden') }, 4500) // load in answers after typing animation
 
-    question.text(questionAnswer[q].question) // question 
-    optionDisplayArr = [answer1Div, answer2Div, answer3Div, answer4Div] // array holding the div info for text
-    correctAnswer = optionDisplayArr[randomizer()]// store correct answer
-    correctAnswer.text(questionAnswer[q].answer)// places the correct answer in div
+        question.text(questionAnswer[q].question) // question 
+        optionDisplayArr = [answer1Div, answer2Div, answer3Div, answer4Div] // array holding the div info for text
+        correctAnswer = optionDisplayArr[randomizer()]// store correct answer
+        correctAnswer.text(questionAnswer[q].answer)// places the correct answer in div
 
-    // incrementer to move through wrongArr and place the wrong answers wherever the real
-    // answer is not displayed in the game
-    w = 0
+        // incrementer to move through wrongArr and place the wrong answers wherever the real
+        // answer is not displayed in the game
+        w = 0
 
-    for (let i=0; i<optionDisplayArr.length; i++) {
-        if (optionDisplayArr[i].text().length < 1) {
-            wrongArr = [questionAnswer[q].wrong1, questionAnswer[q].wrong2, questionAnswer[q].wrong3] // array holding the 3 wrong answers for the setUp() function
-            optionDisplayArr[i].text(wrongArr[w])    // if the div is empty, it gets the first wrong answer stored in the object
-            w++                                      // and then we increment to the next wrong answer
+        for (let i=0; i<optionDisplayArr.length; i++) {
+            if (optionDisplayArr[i].text().length < 1) {
+                wrongArr = [questionAnswer[q].wrong1, questionAnswer[q].wrong2, questionAnswer[q].wrong3] // array holding the 3 wrong answers for the setUp() function
+                optionDisplayArr[i].text(wrongArr[w])    // if the div is empty, it gets the first wrong answer stored in the object
+                w++                                      // and then we increment to the next wrong answer
         }
     }
+
     // on click function for selection
     answer.one().click(function() {
         answer.off('click')// when clicked, click selectors turn off
@@ -113,42 +116,23 @@ var question = $('#question') // question and answer HTML divs
             clearInterval(timer)// timer pause
             setTimeout(function() {softReset()} , 2000) // question reset
         }
-        if (q>5) { // TEST
-            return
-        }
     })
-    }
+}
 
     //timer
     function timerFunc() {
-    timer = setInterval(function() { 
-        time++;  
-        if (time >= 30) {//if time runs out
-        console.log('you lose')
-        clearInterval(timer)
-        lose()
-    };
-    //test time
-    $('#timer').html(30 - time); }, 10000)  
+        timer = setInterval(function() { 
+            time++;  
+            if (time >= 30) {//if time runs out
+            console.log('you lose')
+            clearInterval(timer)
+            gameOver()
+        };
+
+            //test time
+        $('#timer').html(30 - time); }, 200)  
     }
-
-    function endCheck() {
-
-
-        question.addClass('typewriter') // typewriter "game over"
-        setTimeout(function() { answer.removeClass('hidden') }, 500) // show score
-        $('<div>').text('---- GAME OVER ----').appendTo(question);
-        $('<div>').text('> Questions correct: ' + right).appendTo(answer1Div)
-        $('<div>').text('> Questions missed: ' + wrong).appendTo(answer2Div)
-
-        $('<div>').text('Try again?').appendTo(answer3Div)
-        $('<div class="tryAgain yes answerHover d-inline-block">').text('Y').appendTo(answer4Div)
-        $('<div class="tryAgain no answerHover d-inline-block">').text('N').appendTo(answer4Div)
-
-
-
-        
-    }
+    
     //reset for in between questions
     function softReset() {
         w = 0
@@ -166,17 +150,60 @@ var question = $('#question') // question and answer HTML divs
             setTimeout(setUp(), 3000)
             question.addClass('typewriter')
         } else {
-            setTimeout(endCheck(), 3000)
+            setTimeout(gameOver(), 3000)
         }
     }
 
-    function lose() {
-    questionDiv.empty()
-    answer1Div.empty()
-    answer2Div.empty()
-    answer3Div.empty()
-    answer4Div.empty()
-    $('#timer').text("")
-    answer.removeClass('answerHover') 
+    function gameOver() {
+
+        question.empty()
+        answer1Div.empty()
+        answer2Div.empty()
+        answer3Div.empty()
+        answer4Div.empty()
+        display.empty()
+
+        question.addClass('typewriter') // typewriter "game over"
+        setTimeout(function() { answer.removeClass('hidden') }, 4500) // show score
+        $('<div>').text('---- GAME OVER ----').appendTo(question);
+        if (time >= 30) {
+            $('<div>').text('> session expired - timeout').appendTo(answer1Div)
+
+        } else {
+        $('<div>').text('> questions correct: ' + right).appendTo(answer1Div)
+        $('<div>').text('> questions missed: ' + wrong).appendTo(answer2Div) }
+
+        $(answer).off('click')
+        answer.removeClass('answerHover')
+        $('<div>').text('Try again?').appendTo(answer3Div)
+        $('<div class="tryAgain yes answerHover d-inline-block">').text('Y').appendTo(answer4Div)
+        $('<div class="tryAgain no answerHover d-inline-block">').text('N').appendTo(answer4Div)
+        $('.no').on('click', function() {
+            $(document).off('click')
+            console.log('no')
+        })
+        $('.yes').on('click', function() {
+            $(document).off('click')
+            hardReset()
+            console.log('yes')
+        }) 
+    }
+
+    function hardReset() {
+        w=0
+        wrong=0
+        right=0
+        time=0
+
+        question.empty()
+        answer1Div.empty()
+        answer2Div.empty()
+        answer3Div.empty()
+        answer4Div.empty()
+        display.empty()
+        answer.addClass('hidden')
+        answer.removeClass('answerWrong')
+        answer.off('click')
+        setTimeout(start(), 5000)
     }
 })
